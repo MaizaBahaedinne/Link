a850f54db9ce2aa28ad79dc73c19309e9190ae64
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-10-20 22:25:53 +0100

    Update list.php

--- application/views/project/list.php
+++ application/views/project/list.php
@@ -89,91 +89,175 @@
                                     </div>
                             </div><!-- centerl meta -->
                             <div class="col-lg-3">
-                                <!--
-                                <aside class="sidebar static right">
-                                                                      
-                                    <div class="widget">
-                                        <div class="weather-widget low-opacity bluesh">
-                                            <div class="bg-image" style="background-image: url(images/resources/weather.jpg)"></div>
-                                            <span class="refresh-content"><i class="fa fa-refresh"></i></span>
-                                            <div class="weather-week">
-                                                <div class="icon sun-shower">
-                                                  <div class="cloud"></div>
-                                                  <div class="sun">
-                                                    <div class="rays"></div>
-                                                  </div>
-                                                  <div class="rain"></div>
-                                                </div>
-                                            </div>
-                                            <div class="weather-infos">
-                                                <span class="weather-tem">25</span>
-                                                <h3>Cloudy Skyes<i>Sicklervilte, New Jersey</i></h3>
-                                                <div class="weather-date skyblue-bg">
-                                                    <span>MAY<strong>21</strong></span>
-                                                </div>
-                                            </div>
-                                            <div class="monthly-weather">
-                                                <ul>
-                                                    <li>
-                                                        <span>Sun</span>
-                                                        <a href="#" title=""><i class="wi wi-day-sunny"></i></a>
-                                                        <em>40°</em>
-                                                    </li>
-                                                    <li>
-                                                        <span>Mon</span>
-                                                        <a href="#" title=""><i class="wi wi-day-cloudy"></i></a>
-                                                        <em>10°</em>
-                                                    </li>
-                                                    <li>
-                                                        <span>Tue</span>
-                                                        <a href="#" title=""><i class="wi wi-day-hail"></i></a>
-                                                        <em>20°</em>
-                                                    </li>
-                                                    <li>
-                                                        <span>Wed</span>
-                                                        <a href="#" title=""><i class="wi wi-day-lightning"></i></a>
-                                                        <em>34°</em>
-                                                    </li>
-                                                    <li>
-                                                        <span>Thu</span>
-                                                        <a href="#" title=""><i class="wi wi-day-showers"></i></a>
-                                                        <em>22°</em>
-                                                    </li>
-                                                    <li>
-                                                        <span>Fri</span>
-                                                        <a href="#" title=""><i class="wi wi-day-windy"></i></a>
-                                                        <em>26°</em>
-                                                    </li>
-                                                    <li>
-                                                        <span>Sat</span>
-                                                        <a href="#" title=""><i class="wi wi-day-sunny-overcast"></i></a>
-                                                        <em>30°</em>
-                                                    </li>
-                                                </ul>
-                                            </div>
-                                            
-                                        </div>
-                                    </div>
-                                    
-                                    <div class="widget stick-widget">
-                                        <div class="banner medium-opacity purple">
-                                            <div class="bg-image" style="background-image: url(images/resources/baner-widgetbg.jpg)"></div>
-                                            <div class="baner-top">
-                                                <span><img alt="" src="images/book-icon.png"></span>
-                                                <i class="fa fa-ellipsis-h"></i>
-                                            </div>
-                                            <div class="banermeta">
-                                                <p>
-                                                    create your own favourit page.
-                                                </p>
-                                                <span>like them all</span>
-                                                <a data-ripple="" title="" href="#">start now!</a>
-                                            </div>
-                                        </div>                                          
-                                    </div>
-                                </aside>
-                            -->
+                <aside class="sidebar static right">
+                  <div class="widget">
+                    <h4 class="widget-title">Mon Club</h4> 
+                    <div class="your-page">
+                      <figure>
+                        <a href="#" title><img alt="author" src="<?php echo base_url()?>assets/images/resources/profile.png"></a>
+                      </figure>
+                      <div class="page-meta">
+                        <a href="#" title="" class="underline"><?php if ($clubInfo->clubID > 5 ) {echo "club Tunivisions ";}  echo $clubInfo->name ; ?></a>
+                        <span><i class="ti-comment"></i><a href="insight.html" title="">Messages <em>9</em></a></span>
+                        <span><i class="ti-bell"></i><a href="insight.html" title="">Notifications <em>2</em></a></span>
+                      </div>
+                      <ul class="page-publishes">
+                        <li>
+                          <span> <a href="<?php echo base_url()?>Club/clubInfo/<?php echo $clubInfo->clubID ?>">  <i class="ti-pencil-alt"></i>Projets</a></span>
+                        </li>
+                        <li>
+                          <span> 
+                             <a href="<?php echo base_url()?>Club/clubInfo/<?php echo $clubInfo->clubID ?>">
+                                <i class="fa fa-user-plus"></i>Invite
+                            </a>
+                          </span>
+                        </li>
+                      </ul>
+
+
+
+                      <!--
+                      <div class="page-likes">
+                        <ul class="nav nav-tabs likes-btn">
+                          <li class="nav-item"><a class="active" href="#link1" data-toggle="tab" data-ripple="">likes</a></li>
+                           <li class="nav-item"><a class="" href="#link2" data-toggle="tab" data-ripple="">views</a></li>
+                        </ul>
+                        
+                        <div class="tab-content">
+                          <div class="tab-pane active fade show" id="link1">
+                          <span><i class="ti-heart"></i>884</span>
+                            <a href="#" title="weekly-likes">35 new likes this week</a>
+                            <div class="users-thumb-list">
+                            <a href="#" title="" data-toggle="tooltip" data-original-title="Anderw">
+                              <img src="images/resources/userlist-1.jpg" alt="">  
+                            </a>
+                            <a href="#" title="" data-toggle="tooltip" data-original-title="frank">
+                              <img src="images/resources/userlist-2.jpg" alt="">  
+                            </a>
+                            <a href="#" title="" data-toggle="tooltip" data-original-title="Sara">
+                              <img src="images/resources/userlist-3.jpg" alt="">  
+                            </a>
+                            <a href="#" title="" data-toggle="tooltip" data-original-title="Amy">
+                              <img src="images/resources/userlist-4.jpg" alt="">  
+                            </a>
+                            <a href="#" title="" data-toggle="tooltip" data-original-title="Ema">
+                              <img src="images/resources/userlist-5.jpg" alt="">  
+                            </a>
+                            <a href="#" title="" data-toggle="tooltip" data-original-title="Sophie">
+                              <img src="images/resources/userlist-6.jpg" alt="">  
+                            </a>
+                            <a href="#" title="" data-toggle="tooltip" data-original-title="Maria">
+                              <img src="images/resources/userlist-7.jpg" alt="">  
+                            </a>  
                             </div>
+                          </div>
+                          <div class="tab-pane fade" id="link2">
+                            <span><i class="fa fa-eye"></i>440</span>
+                            <a href="#" title="weekly-likes">440 new views this week</a>
+                            <div class="users-thumb-list">
+                            <a href="#" title="" data-toggle="tooltip" data-original-title="Anderw">
+                              <img src="images/resources/userlist-1.jpg" alt="">  
+                            </a>
+                            <a href="#" title="" data-toggle="tooltip" data-original-title="frank">
+                              <img src="images/resources/userlist-2.jpg" alt="">  
+                            </a>
+                            <a href="#" title="" data-toggle="tooltip" data-original-title="Sara">
+                              <img src="images/resources/userlist-3.jpg" alt="">  
+                            </a>
+                            <a href="#" title="" data-toggle="tooltip" data-original-title="Amy">
+                              <img src="images/resources/userlist-4.jpg" alt="">  
+                            </a>
+                            <a href="#" title="" data-toggle="tooltip" data-original-title="Ema">
+                              <img src="images/resources/userlist-5.jpg" alt="">  
+                            </a>
+                            <a href="#" title="" data-toggle="tooltip" data-original-title="Sophie">
+                              <img src="images/resources/userlist-6.jpg" alt="">  
+                            </a>
+                            <a href="#" title="" data-toggle="tooltip" data-original-title="Maria">
+                              <img src="images/resources/userlist-7.jpg" alt="">  
+                            </a>  
+                            </div>
+                          </div>  
+                        </div>
+
+                      </div>-->
+                    </div>
+                  </div><!-- page like widget -->
+                  <!-- explore events -->
+                  <div class="widget">
+                    <h4 class="widget-title">Calendar</h4>
+                    
+                    <div class="full-calendar">
+                                <div id="yourId" class="jalendar mid">
+                                  <?php foreach ($taches as $tache ) { 
+
+                                    $date= date_create($tache->deadline); 
+                                    $dateF =date_format($date,'d-m-Y');
+                                    $timeF =date_format($date,'H:i'); 
+                                     ?> 
+
+                                    <div class="added-event"
+                                    data-date="<?php echo  $dateF ?>" 
+                                    data-time="<?php echo  $timeF ?>"
+                                    data-link="" 
+                                    data-title="Tache : <?php echo  $tache->titre ?>">
+                                      
+                                    </div>
+                                
+                                  <?php }?>
+
+                                  <?php foreach ($Projets as $Projet ) { 
+
+                                    $date= date_create($Projet->startDate); 
+                                    $dateF =date_format($date,'d-m-Y');   
+                                    $timeF =date_format($date,'H:i');   
+                                     ?> 
+
+                                    <div class="added-event"
+                                    data-date="<?php echo  $dateF ?>" 
+                                    data-time="<?php echo  $timeF ?>"
+                                    data-link="" 
+                                    data-title="<?php echo  $Projet->type ?> : <?php echo  $Projet->titre ?>">
+                                      
+                                    </div>
+                                
+                                  <?php }?>
+
+                                </div>
+                              </div>
+                  </div><!-- calendar -->
+                  <!--
+                  <div class="widget stick-widget" style="">
+                    <h4 class="widget-title">Recent Links <a title="" href="#" class="see-all">See All</a></h4>
+                    <ul class="recent-links">
+                      <li>
+                        <figure><img src="images/resources/recentlink-1.jpg" alt=""></figure>
+                        <div class="re-links-meta">
+                          <h6><a href="#" title="">moira's fade reaches much farther than you think.</a></h6>
+                          <span>2 weeks ago </span>
+                        </div>
+                      </li>
+                      <li>
+                        <figure><img src="images/resources/recentlink-2.jpg" alt=""></figure>
+                        <div class="re-links-meta">
+                          <h6><a href="#" title="">daniel asks if we want him to do the voice of doomfist</a></h6>
+                          <span>3 months ago </span>
+                        </div>
+                      </li>
+                      <li>
+                        <figure><img src="images/resources/recentlink-3.jpg" alt=""></figure>
+                        <div class="re-links-meta">
+                          <h6><a href="#" title="">the pitnik overwatch scandals.</a></h6>
+                          <span>1 day before</span>
+                        </div>
+                      </li>
+                    </ul>
+                  </div>
+                  -->
+                  <!-- recent links -->
+
+                </aside>
+              </div><!-- sidebar -->
                         </div>  
                     </div>
                 </div>
