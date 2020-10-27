7283bac3fcf768e79a8e909896c0e2003f874ad0
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-10-27 15:01:28 +0100

    Update footer.php

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -357,7 +357,7 @@
 
     <script>
     tinymce.init({
-      selector: 'textarea',
+      selector: '#TinyMCE',
       plugins: 'a11ychecker advcode casechange formatpainter linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tinycomments tinymcespellchecker',
       toolbar: 'a11ycheck addcomment showcomments casechange checklist code formatpainter pageembed permanentpen table',
       toolbar_mode: 'floating',
