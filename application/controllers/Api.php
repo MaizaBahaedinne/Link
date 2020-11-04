e5300eccbda74d5a38e5ea002fe531d50e717cdf
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-11-05 00:38:28 +0100

    web services

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -56,4 +56,24 @@ class Api extends BaseController
 
 
 
+    public function Hierarchy ()
+        {
+                
+                $data["President"]  =   $this->user_model->getMemberByRoleAndCelulle(-1 , 1 , '' ) ;
+                $data["EM"]     =   $this->user_model->getMemberByRoleAndCelulle(-1 , 10 , '' ) ;
+                $data["CU"]     =   $this->user_model->getMemberByRoleAndCelulle(0 , 2 , '' ) ;
+                $data["CU"]->managers   =   $this->user_model->getManagers(0); 
+                $data["CH"]     =   $this->user_model->getMemberByRoleAndCelulle(1 , 2 , '' ) ;
+                $data["CH"]->managers   =   $this->user_model->getManagers(1);
+                $data["CA"]     =   $this->user_model->getMemberByRoleAndCelulle(2 , 2 , '' ) ;
+                $data["CA"]->managers   =   $this->user_model->getManagers(2);
+
+                $this->global['pageTitle'] = 'Clubs';
+                $this->global['active'] = 'Clubs';
+               $this->response($data);   
+                
+        }
+
+
+
 }
