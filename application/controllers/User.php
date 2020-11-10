9640769af05632f3e453cb6c57a7b2d11e8eeb46
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-11-10 23:47:30 +0100

    mail

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -528,52 +528,6 @@ class User extends BaseController
     }
 
 
-
-    public function send_mail($title,$mailContent,$addresse,$name)
-            {
-                // Load PHPMailer library
-                    $this->load->library('phpmailer_lib');
-                    
-                    // PHPMailer object
-                    $mail = $this->phpmailer_lib->load();
-                    
-                    // SMTP configuration
-                    $mail->isSMTP();
-                    $mail->Host     = 'tunivisions.link';
-                    $mail->SMTPAuth = true;
-                    $mail->Username = 'no-reply@tunivisions.link';
-                    $mail->Password = 'Tunivisions-Link-2019';
-                    $mail->SMTPSecure = 'tls';
-                    $mail->Port     = 587;
-                    
-                    $mail->setFrom('no-reply@tunivisions.link', 'Tunivisions Link');
-                    $mail->addReplyTo('no-reply@tunivisions.link', 'Tunivisions Link');
-                    
-                    // Add a recipient
-                
-                    $mail->addAddress($to);
-                    
-                    
-                    // Email subject
-                    $mail->Subject = $title ;
-                    
-                    // Set email format to HTML
-                    $mail->isHTML(true);
-                    
-                    // Email body content
-                    $data['name'] =  $name ; 
-                    $mail->Body = $this->load->view("mail/bienvenue" , $data );;
-                    
-                    // Send email
-                    if(!$mail->send()){
-                        echo 'Message could not be sent.';
-                        echo 'Mailer Error: ' . $mail->ErrorInfo;
-                    }else{
-                        echo 'Message has been sent';
-                    }
-                       
-            }
-
             
     /**
      * Page not found : error 404
