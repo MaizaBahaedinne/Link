0d348c956a03812d016f25cadc02f22ff8fcd2f4
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-11-10 12:52:00 +0100

    Update header.php

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -97,11 +97,33 @@
   window.fbAsyncInit = function() {
     FB.init({
       appId      : '2993377220885022',
+      cookie     : true,
       xfbml      : true,
       version    : 'v8.0'
     });
     FB.AppEvents.logPageView();
   };
+
+  // Check whether the user already logged in
+    FB.getLoginStatus(function(response) {
+        if (response.status === 'connected') {
+            //display user data
+            getFbUserData();
+             $('#facebookPageGroup').show() ;
+ 			$('#facebookAuth').hide() ; 
+        }
+    });
+
+    // Fetch the user profile data from facebook
+function getFbUserData(){
+    FB.api('/me', {locale: 'en_US', fields: 'id,first_name,last_name,email,link,gender,locale,picture'},
+    function (response) {
+        document.getElementById('fbLink').setAttribute("onclick","fbLogout()");
+        document.getElementById('fbLink').innerHTML = 'Logout from Facebook';
+        document.getElementById('status').innerHTML = '<p>Thanks for logging in, ' + response.first_name + '!</p>';
+        document.getElementById('userData').innerHTML = '<h2>Facebook Profile Details</h2><p><img src="'+response.picture.data.url+'"/></p><p><b>FB ID:</b> '+response.id+'</p><p><b>Name:</b> '+response.first_name+' '+response.last_name+'</p><p><b>Email:</b> '+response.email+'</p><p><b>Gender:</b> '+response.gender+'</p><p><b>FB Profile:</b> <a target="_blank" href="'+response.link+'">click to view profile</a></p>';
+    });
+}
 
   (function(d, s, id){
      var js, fjs = d.getElementsByTagName(s)[0];
@@ -117,8 +139,7 @@
   'GET',
   {"fields":"id,last_name,birthday,link,location,groups,likes"},
   function(response) {
-      $('#facebookPageGroup').show() ;
- 		$('#facebookAuth').hide() ; 
+     
        }
 );
 
