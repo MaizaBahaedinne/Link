57c247cfbcd6aa2d8f05991d50e24aab3d189b2b
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-11-10 23:43:48 +0100

    Update Register.php

--- application/controllers/Register.php
+++ application/controllers/Register.php
@@ -7,7 +7,7 @@
  * @version : 1.1
  * @since : 15 November 2016
  */
-class Register extends CI_Controller
+class Register extends BaseController
 {
     /**
      * This is default constructor of the class
