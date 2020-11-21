515f1badd5ba342066c45b6268eca2c0dd0ca556
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2020-11-21 13:38:06 +0100

    Update footer.php

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -67,11 +67,11 @@
           <div class="widget">
             <div class="widget-title"><h4>Equipe</h4></div>
             <ul class="colla-apps">
-              <!--
+              
               <li><a href="https://tunivisions.link/User/ProfileShow/2"><img src="<?php echo base_url()?>uploads/avatar/avatar__MAIZA,P20Bahaedinne_IMG_1257.jpg" class="alligator-profile-likes"> Bahaedinne Maiza</a></li>
               <li><a href="https://tunivisions.link/User/ProfileShow/4320"><img src="<?php echo base_url()?>uploads/avatar/FB_IMG_1581494137538.jpg27338373" class="alligator-profile-likes"> Ayman Karray</a></li>
               <li><a href="https://tunivisions.link/User/ProfileShow/4875"><img src="<?php echo base_url()?>uploads/avatar/inbound4907866068062005621.jpg27847992" class="alligator-profile-likes"> Imen Abbassi</a></li>
-              -->
+              
             </ul>
           </div>
         </div>
