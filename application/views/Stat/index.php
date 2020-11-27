92aeeefa9f52f9d988ce2580568becd09703cfa5
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-11-28 00:13:08 +0100

    Update index.php

--- application/views/Stat/index.php
+++ application/views/Stat/index.php
@@ -21,7 +21,34 @@
 					<div class="info-section">
 						<div class="row">
 										
-										
+							<div class="col-lg-4 col-md-6 col-sm-6">
+								<div class="stat-box">
+									<i class="fa fa-user"></i>
+									<div class="anlytc-meta">
+										<h4><?php $tuni = (count($TunimateursA) + count($TunimateursHS) + count($TunimateursU)) ; echo $tuni ;  ?></h4>
+										<p>40%</p>
+										<span>Tunimateurs</span> 
+									</div>
+									<div class="progress" style="height:6px">
+										<div class="progress-bar bg-valvot" style="width:60%"></div>
+									</div> 
+								</div>
+							</div>
+
+							<div class="col-lg-4 col-md-6 col-sm-6">
+								<div class="stat-box">
+									<i class="fas fa-users"></i>
+									<div class="anlytc-meta">
+										<h4><?php  $clubs = (count($clubsU) + count($clubsHS)) ; echo $clubs ;  ?></h4>
+										<p>40%</p>
+										<span>Clubs </span> 
+									</div>
+									<div class="progress" style="height:6px">
+										<div class="progress-bar bg-valvot" style="width:60%"></div>
+									</div> 
+								</div>
+							</div>
+
 							<div class="col-lg-4 col-md-6 col-sm-6">
 								<div class="stat-box">
 									<i class="fas fa-project-diagram"></i>
@@ -36,33 +63,7 @@
 								</div>
 							</div>
 
-							<div class="col-lg-4 col-md-6 col-sm-6">
-								<div class="stat-box">
-									<i class="fa fa-user"></i>
-									<div class="anlytc-meta">
-										<h4><?php echo (count($TunimateursA) + count($TunimateursHS) + count($TunimateursU)) ?></h4>
-										<p>40%</p>
-										<span>Tunimateurs</span> 
-									</div>
-									<div class="progress" style="height:6px">
-										<div class="progress-bar bg-valvot" style="width:60%"></div>
-									</div> 
-								</div>
-							</div>
 
-							<div class="col-lg-4 col-md-6 col-sm-6">
-								<div class="stat-box">
-									<i class="fas fa-users"></i>
-									<div class="anlytc-meta">
-										<h4><?php echo (count($clubsU) + count($clubsHS))  ?></h4>
-										<p>40%</p>
-										<span>Clubs </span> 
-									</div>
-									<div class="progress" style="height:6px">
-										<div class="progress-bar bg-valvot" style="width:60%"></div>
-									</div> 
-								</div>
-							</div>
 
 
 
@@ -70,4 +71,63 @@
 					</div>
 				</div>
 
-</div></div></div></div></div></section>
+
+				<div class="col-lg-3">
+								<aside class="sidebar static">
+									<div class="widget">
+									<h4 class="widget-title">Membres Alumni</h4>
+										<div class="static-meta"> 
+											<i class="fa fa-user"></i>
+											<div class="info-meta">
+												<h4><?php  echo count($TunimateursA) ?></h4>
+												<i>30%</i>
+											</div>
+											<div class="spark">
+				                            <div class="sparkline12"><canvas width="158" height="40" style="display: inline-block; width: 158px; height: 40px; vertical-align: top;"></canvas></div>
+				                          </div>
+										</div>
+									</div>
+									<div class="widget">
+									<h4 class="widget-title">Membres University</h4>
+										<div class="static-meta"> 
+											<i class="fa fa-user"></i>
+											<div class="info-meta">
+												<h4><?php  echo count($TunimateursU) ?></h4>
+												<i>30%</i>
+											</div>
+											<div class="spark">
+				                            <div class="sparkline12"><canvas width="158" height="40" style="display: inline-block; width: 158px; height: 40px; vertical-align: top;"></canvas></div>
+				                          </div>
+										</div>
+									</div>
+									<div class="widget">
+									<h4 class="widget-title">Membres High School</h4>
+										<span>This week</span>
+										<div class="static-meta"> 
+											<i class="fa fa-user"></i>
+											<div class="info-meta">
+												<h4><?php  echo count($TunimateursHS) ?></h4>
+												<i>10%</i>
+											</div>
+											<div class="spark">
+				                            <div class="sparkline_three"><canvas width="200" height="40" style="display: inline-block; width: 200px; height: 40px; vertical-align: top;"></canvas></div>
+				                          </div>
+										</div>
+									</div>
+																		
+								</aside>
+							</div>
+
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</section>
+
+
+
+	
+    <script src="<?php echo base_url() ?>assets/js/echarts.min.js"></script>
+    <script src="<?php echo base_url() ?>assets/js/world.js"></script>
+    <script src="<?php echo base_url() ?>assets/js/jquery.sparkline.min.js"></script> 
