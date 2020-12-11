8bb04398461c079f057204fa93eea449a54f1011
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2020-12-11 17:23:49 +0100

    Update Tfm_part_model.php

--- application/models/Tfm_part_model.php
+++ application/models/Tfm_part_model.php
@@ -71,6 +71,7 @@ class Tfm_part_model extends CI_Model
     {
         $this->db->select(' count(BaseTbl.id) nbr , BaseTbl.dateInscrip ');
         $this->db->from('tbl_tfm_part as BaseTbl');
+        $this->db->where('BaseTbl.tfmId =',$projetId );
         $this->db->group_by('  hour( BaseTbl.dateInscrip ) ');
         $this->db->order_by('   BaseTbl.dateInscrip   ');
  
