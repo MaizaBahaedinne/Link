9c75745272ba152202c3aa11aefdb40123e5dd94
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2020-12-11 16:45:05 +0100

    Update statistique.php

--- application/views/project/statistique.php
+++ application/views/project/statistique.php
@@ -81,7 +81,7 @@
 								<div class="col-lg-12">
 								<aside class="sidebar ">
 									<div class="widget">
-		                                        <h4 class="widget-title"></h4>
+		                                        <h4 class="widget-title">Rythme des inscription</h4>
 		                                        <div class="your-page">	
 													<canvas id="timeSeriesChart" > </canvas>
 													<script>
