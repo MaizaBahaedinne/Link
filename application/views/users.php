fddd523fca0a6be57bfc6dbd425e0305ce4cdeb4
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2020-12-12 17:08:37 +0100

    Update users.php

--- application/views/users.php
+++ application/views/users.php
@@ -1,38 +1,23 @@
-<?php if ($SA== 1  ) {  ?>
-<div id="content-page" class="content-page">
-            <div class="container">
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.css">
-<script defer type="text/javascript"  src="https://code.jquery.com/jquery-3.5.1.js"></script>  
-<script defer type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.js"></script>
+<?php if ($SA== 1 && $SA == 2   ) {  ?>
+<section>
+    <div class="gap gray-bg">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="row widget-page merged20">
+              <div class="col-lg-12 col-md-12 col-sm-6">
+                <aside class="sidebar">
+                   <div class="widget">
+                    <h4 class="widget-title">Liste des clubs 
+                       <?php if($SA == 1 || $SA == 2  ){ ?>
+                      <a class="align-right main-btn" data-toggle="modal" data-target="#myModal" >Ajouter un club</a>
+                      <?php } ?> 
+                       </h4>
 
-
-
-<style type="text/css">
-.content-loader tr td 
-{
-    white-space: nowrap;
-}
-</style>
-
-
-
-
-
-                <nav class="page-breadcrumb">
-                    <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="#">Systeme</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">utilisateurs</li>
-                    </ol>
-                </nav>
-
-                <div class="row">
-                   
-            <div class="card col-md-12">
-              <div class="card-body">
-                                <h6 class="card-title"></h6>
+                    <ul class="faved-page">
                                    
                 
-                    <table  class="display table dataTable no-footer" id="tableid" style="width: cover" >
+                    <table  class="table table-striped table-responsive-xl" id="tableid" style="width: cover" >
                     <thead>
                     <tr>
                        
@@ -140,22 +125,16 @@
 
                   </table>
 
-
-               
-              </div>
-            </div>
-                
+                    </ul>
                 </div>
-
-
-             <!-- begin::Global Config(global config for global JS sciprts) -->
-            <link rel="stylesheet" href="<?php echo base_url() ?>assets/vendors/datatables.net-bs4/dataTables.bootstrap4.css">
-  <script defer src="<?php echo base_url() ?>assets/vendors/datatables.net/jquery.dataTables.js"></script>
-  <script defer src="<?php echo base_url() ?>assets/vendors/datatables.net-bs4/dataTables.bootstrap4.js"></script>
-  <script type="text/javascript">
-            $(document).ready( function () {
-                $('#example').DataTable();
-            } );
-  </script>
-</div></div>
+            </aside>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+</div>
+</section>
+               
+            
 <?php } ?>
