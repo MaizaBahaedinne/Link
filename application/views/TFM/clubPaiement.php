03b40009f5c165afda0bd0bc829e03f3075a572d
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-12-12 23:31:12 +0100

    Update clubPaiement.php

--- application/views/TFM/clubPaiement.php
+++ application/views/TFM/clubPaiement.php
@@ -13,7 +13,7 @@
                     </h4>
             <!--begin: Datatable -->
                 <ul class="faved-page">
-                    <table id="example" class="table dataTable no-footer" style="width:cover" >
+                    <table class="table table-striped table-responsive-xl" id="tableid" style="width: cover" >
                                 <thead>
                                 <tr>
                                     <th>Club</th>
@@ -30,38 +30,16 @@
                                 ?>
                                 <tr>
                                     <td>
-
-                                        <span style="width: 145px;">
-                                            <div class="kt-user-card-v2">
-                                     
-                                                <div class="kt-user-card-v2__details">                              
-                                                    <a class="kt-user-card-v2__name" href="#">
-                                                     <?php
-                                                        if ($record->ClubName != "Foundation"  && $record->ClubName != "Foundation Junior" && $record->ClubName != "Alumni")
-                                                        echo "club ";
-                                                        ?>   Tunivisions  <?php echo $record->ClubName ?>
-                                                    </a>                              
-                                                </div>                      
-                                            </div>
-                                        </span>
-
-
+                                        <?php echo $record->ClubName ?>          
                                     </td>
-                                    
-                                 
-                                
-                                    
-                                <td>
-
-                                  <?php echo $record->partant ?> 
-                                </td> 
-             
-
-                                <td> 
-                                      <?php if ( $clubID == 0 || $role == 10 ||  $uid == 2 || $SA == 1 || $SA == 2   ) { ?>
-                                            <a href="<?php echo base_url() ?>TFM/PaimentByClub/<?php echo $record->clubID ?>/<?php echo $projectId ?>" class="btn btn-sm btn-primary" >Paiement</a>
-                                     <?php } ?>   
-                                 </td>
+                                    <td>
+                                      <?php echo $record->partant ?> 
+                                    </td> 
+                                    <td> 
+                                          <?php if ( $clubID == 0 || $role == 10 ||  $uid == 2 || $SA == 1 || $SA == 2   ) { ?>
+                                                <a href="<?php echo base_url() ?>TFM/PaimentByClub/<?php echo $record->clubID ?>/<?php echo $projectId ?>" class="btn btn-sm btn-primary" >Paiement</a>
+                                         <?php } ?>   
+                                    </td>
                                 </tr>
 
                                 <?php
