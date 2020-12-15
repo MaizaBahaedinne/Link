61a672205f8579b13535ef7d0af0662bf0f5bb39
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2020-12-15 16:02:10 +0100

    commuit

--- application/controllers/Statistique.php
+++ application/controllers/Statistique.php
@@ -41,6 +41,7 @@ class Statistique extends BaseController {
         $data['countRythme'] = $this->Tfm_part_model->TFMPartRythme($ProjectId);
         $data['countINSNotConfirmed'] = $this->Tfm_part_model->TFMPartListing($ProjectId,2);
         $data['countINSConfirmed'] = $this->Tfm_part_model->TFMPartListing($ProjectId,1);
+        $data['countINSConfirmed1'] = $this->Tfm_part_model->TFMPartListing1($ProjectId,2);
     
 
 
