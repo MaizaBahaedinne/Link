ebc17b94bcb691fb3d728eb8464000db96211945
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2020-12-15 15:11:32 +0100

    Update Tfm_part_model.php

--- application/models/Tfm_part_model.php
+++ application/models/Tfm_part_model.php
@@ -273,7 +273,7 @@ class Tfm_part_model extends CI_Model
        
         $this->db->join('tbl_users as Usersr1', 'Usersr1.userId = BaseTbl.p_tranch1', 'LEFT');
         
-        $this->db->where('BaseTbl.tfmId = '. $projectId.' BaseTbl.statut = ','1');
+        $this->db->where('BaseTbl.tfmId = '. $projectId.' and  BaseTbl.statut = ','1');
         $this->db->where('Users.clubID =', $clubId);    
 
         $query = $this->db->get();
