fc9e9f4a7809b95def7fa7f3810a7970a9e6739b
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2020-12-15 14:50:24 +0100

    Update PaimentByClub.php

--- application/views/TFM/PaimentByClub.php
+++ application/views/TFM/PaimentByClub.php
@@ -17,12 +17,12 @@
 <div class="row">
 	
 
-	<div class="card col-md-6">
+	<div class="card col-md-4">
 		<div class="card">
 
 		<form action="<?php echo base_url() ?>TFM/partanTfmPaiement1/<?php echo $clubID ?>/<?php echo $projectId ?>" method="post">
 			<div class="card-header">
-				<h5>Liste des membre participant au TFM | <?php echo count($userRecords) ?> </h5>		
+				<h5>Tranche 1 | <?php echo count($userRecords) ?> </h5>		
 			</div>
 			<div class="card-body">
 				
@@ -30,7 +30,40 @@
 					<div class="form-check form-check-flat form-check-primary">
 						<label class="form-check-label">
 							<?php if($uid == 2 || $uid ==  181 || $uid ==  556 
-							                   || $uid ==  1031 || $uid ==  2616   ){ ?>
+							                   || $uid ==  1031 || $uid ==  2616 || $uid ==  237    ){ ?>
+							<input class="form-check-input" type="checkbox" name="participant[]" value="<?php echo $record->id ; ?>" >
+							<?php } ?> <?php echo ' '.$record->name ;  ?><small><small> #<?php echo ' '.$record->id ;  ?></small></small>
+						</label>
+					</div>
+				<?php } ?>
+
+
+
+				
+			</div>
+			<div class="card-footer">
+				<input type="submit" class="btn btn-primary" value="Valider">
+				<input type="reset" class="btn btn-danger" value="Anuler">
+			</div>
+			</form>
+		</div>
+	</div>
+
+
+	<div class="card col-md-4">
+		<div class="card">
+
+		<form action="<?php echo base_url() ?>TFM/partanTfmPaiement2/<?php echo $clubID ?>/<?php echo $projectId ?>" method="post">
+			<div class="card-header">
+				<h5>Tranche 2  | <?php echo count($userRecords2) ?> </h5>		
+			</div>
+			<div class="card-body">
+				
+				<?php foreach ($userRecords2 as $record ) { ?>
+					<div class="form-check form-check-flat form-check-primary">
+						<label class="form-check-label">
+							<?php if($uid == 2 || $uid ==  181 || $uid ==  556 
+							                   || $uid ==  1031 || $uid ==  2616 || $uid ==  237    ){ ?>
 							<input class="form-check-input" type="checkbox" name="participant[]" value="<?php echo $record->id ; ?>" >
 							<?php } ?> <?php echo ' '.$record->name ;  ?><small><small> #<?php echo ' '.$record->id ;  ?></small></small>
 						</label>
@@ -51,7 +84,7 @@
 	
 	
 
-	<div class="card col-md-6">
+	<div class="card col-md-4">
 		<div class="card">
 			
 			<div class="card-header">
