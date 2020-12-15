60f25064f879e8f884ace0639c0ee0c31fd2d1b0
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2020-12-15 15:59:41 +0100

    commit

--- application/models/Tfm_part_model.php
+++ application/models/Tfm_part_model.php
@@ -36,7 +36,7 @@ class Tfm_part_model extends CI_Model
      * @param number $segment : This is pagination limit
      * @return array $result : This is result
      */
-    function TFMPartListing($projetId , $statut = '',$part = 2 )
+    function TFMPartListing($projetId , $statut = '' )
     {
         $this->db->select(' Users.userId ,BaseTbl.id , Users.name , Clubs.name ClubName , Role.role , Users.cellule , Users.gouvernorat , BaseTbl.p_tranch1 , BaseTbl.p_tranch2 , BaseTbl.moto , BaseTbl.sysMobile , BaseTbl.statut , Usersr1.name as recp1 , BaseTbl.dateP_tranch1 as dateTranche1 , Usersr2.name as recp2 , BaseTbl.dateP_tranch2 as dateTranche2 , BaseTbl.dateInscrip ');
         $this->db->from('tbl_tfm_part as BaseTbl');
