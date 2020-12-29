1956f01e052106273d90a773f3c766dd2f30aa58
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-12-29 21:53:30 +0100

    Update footer.php

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -367,6 +367,7 @@
                   
                   $.ajax( "<?php echo base_url() ?>"+qrCodeMessage+"/<?php echo $uid ?>" )
                         .done(function() {
+                          alert("<?php echo base_url() ?>"+qrCodeMessage+"/<?php echo $uid ?>") ;
                           alert( "Votre participation a été validée avec succée" );
                           alert( "On va actuliser cette page" );
                           location.reload();
