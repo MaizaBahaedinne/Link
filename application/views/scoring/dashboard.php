db2da1a2f55302d2f3f5b5c0deda8bfbc7abd75b
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2021-01-17 21:20:38 +0100

    Update dashboard.php

--- application/views/scoring/dashboard.php
+++ application/views/scoring/dashboard.php
@@ -149,6 +149,239 @@
                         	</div>
                         	<!-- end scoring clubs -->
 
+
+
+                            <!-- scoring membres Marketing -->
+                            <div class="col-lg-3 " >
+                                <aside class="sidebar static right" >
+                                <div class="widget">
+                                <h4 class="widget-title">TOP 10 Tunimateurs</h4>
+                                 <style type="text/css">
+                                        .pro-turtle {
+                                            object-fit: cover;
+                                            border-radius : 100% ;
+                                            object-position: 50% 50%;
+                                            width: 30px;
+                                            height: 30px;
+                                        }
+                                    </style> 
+                                    <table  class="table table-striped table-responsive" id="tableid" style="width: cover" >
+                                      <thead>
+                                          <th width="5%" >#
+                                          </th>
+                                          <th width="10%" >
+                                            Badge
+                                          </th>
+                                          <th>
+                                            Tunimateur
+                                          </th>
+                                          <th>
+                                            Club
+                                          </th>
+                                          
+                                     
+                                          <th>
+                                            Score
+                                          </th>
+                                          </thead>
+                                    <?php  if(!empty($mM)){ $c=1 ;   foreach ($mM as $membre ) { ?>
+                                      <tr>
+
+                                        <td  ><h4> <?php echo   $c   ?> </h4> </td>
+
+                                    
+                                        <td>
+                                             <?php   if ($c== 1) { ?>
+
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 7a8 8 0 1 1 0 16 8 8 0 0 1 0-16zm0 3.5l-1.323 2.68-2.957.43 2.14 2.085-.505 2.946L12 17.25l2.645 1.39-.505-2.945 2.14-2.086-2.957-.43L12 10.5zm1-8.501L18 2v3l-1.363 1.138A9.935 9.935 0 0 0 13 5.049L13 2zm-2 0v3.05a9.935 9.935 0 0 0-3.636 1.088L6 5V2l5-.001z" fill="rgba(241,196,14,1)"/></svg>
+
+                                            <?php } else if ($c== 2) { ?>
+
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 7a8 8 0 1 1 0 16 8 8 0 0 1 0-16zm0 3.5l-1.323 2.68-2.957.43 2.14 2.085-.505 2.946L12 17.25l2.645 1.39-.505-2.945 2.14-2.086-2.957-.43L12 10.5zm1-8.501L18 2v3l-1.363 1.138A9.935 9.935 0 0 0 13 5.049L13 2zm-2 0v3.05a9.935 9.935 0 0 0-3.636 1.088L6 5V2l5-.001z" fill="rgba(149,164,166,1)"/></svg>
+
+                                            <?php } else if ($c== 3) { ?>
+
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 7a8 8 0 1 1 0 16 8 8 0 0 1 0-16zm0 3.5l-1.323 2.68-2.957.43 2.14 2.085-.505 2.946L12 17.25l2.645 1.39-.505-2.945 2.14-2.086-2.957-.43L12 10.5zm1-8.501L18 2v3l-1.363 1.138A9.935 9.935 0 0 0 13 5.049L13 2zm-2 0v3.05a9.935 9.935 0 0 0-3.636 1.088L6 5V2l5-.001z" fill="rgba(155,77,9,1)"/></svg>
+
+                                            <?php   }  ?>
+                                        </td>
+                                         
+
+                                         <td>
+                                      
+                                         <a href="<?php echo base_url() ?>User/ProfileShow/<?php echo $membre->userId ?>"> <img src="<?php echo base_url() ?>uploads/avatar/<?php echo $membre->avatar ?>" class="pro-turtle " alt="user">
+                                            <small><?php echo $membre->name ?></small>                        
+                                        </td>
+                                        <td>
+                                            <small><?php echo $membre->club ?></small>
+                                        </td>
+
+                                        <td> <b ><?php echo $membre->scores ?></b> </td>
+                                      </tr>
+                                      
+                                 
+                                    <?php $c= $c + 1 ;  }   }else { echo "Il y a pas de données";} ?>
+                                </table>
+                            </div>
+                            </aside>
+                            </div>
+
+                            <!-- end scoring membres Marketing -->
+
+                            <!-- scoring membres Event -->
+                            <div class="col-lg-3 " >
+                                <aside class="sidebar static right" >
+                                <div class="widget">
+                                <h4 class="widget-title">TOP 10 Tunimateurs</h4>
+                                 <style type="text/css">
+                                        .pro-turtle {
+                                            object-fit: cover;
+                                            border-radius : 100% ;
+                                            object-position: 50% 50%;
+                                            width: 30px;
+                                            height: 30px;
+                                        }
+                                    </style> 
+                                    <table  class="table table-striped table-responsive" id="tableid" style="width: cover" >
+                                      <thead>
+                                          <th width="5%" >#
+                                          </th>
+                                          <th width="10%" >
+                                            Badge
+                                          </th>
+                                          <th>
+                                            Tunimateur
+                                          </th>
+                                          <th>
+                                            Club
+                                          </th>
+                                          
+                                     
+                                          <th>
+                                            Score
+                                          </th>
+                                          </thead>
+                                    <?php  if(!empty($mE)){ $c=1 ;   foreach ($mE as $membre ) { ?>
+                                      <tr>
+
+                                        <td  ><h4> <?php echo   $c   ?> </h4> </td>
+
+                                    
+                                        <td>
+                                             <?php   if ($c== 1) { ?>
+
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 7a8 8 0 1 1 0 16 8 8 0 0 1 0-16zm0 3.5l-1.323 2.68-2.957.43 2.14 2.085-.505 2.946L12 17.25l2.645 1.39-.505-2.945 2.14-2.086-2.957-.43L12 10.5zm1-8.501L18 2v3l-1.363 1.138A9.935 9.935 0 0 0 13 5.049L13 2zm-2 0v3.05a9.935 9.935 0 0 0-3.636 1.088L6 5V2l5-.001z" fill="rgba(241,196,14,1)"/></svg>
+
+                                            <?php } else if ($c== 2) { ?>
+
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 7a8 8 0 1 1 0 16 8 8 0 0 1 0-16zm0 3.5l-1.323 2.68-2.957.43 2.14 2.085-.505 2.946L12 17.25l2.645 1.39-.505-2.945 2.14-2.086-2.957-.43L12 10.5zm1-8.501L18 2v3l-1.363 1.138A9.935 9.935 0 0 0 13 5.049L13 2zm-2 0v3.05a9.935 9.935 0 0 0-3.636 1.088L6 5V2l5-.001z" fill="rgba(149,164,166,1)"/></svg>
+
+                                            <?php } else if ($c== 3) { ?>
+
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 7a8 8 0 1 1 0 16 8 8 0 0 1 0-16zm0 3.5l-1.323 2.68-2.957.43 2.14 2.085-.505 2.946L12 17.25l2.645 1.39-.505-2.945 2.14-2.086-2.957-.43L12 10.5zm1-8.501L18 2v3l-1.363 1.138A9.935 9.935 0 0 0 13 5.049L13 2zm-2 0v3.05a9.935 9.935 0 0 0-3.636 1.088L6 5V2l5-.001z" fill="rgba(155,77,9,1)"/></svg>
+
+                                            <?php   }  ?>
+                                        </td>
+                                         
+
+                                         <td>
+                                      
+                                         <a href="<?php echo base_url() ?>User/ProfileShow/<?php echo $membre->userId ?>"> <img src="<?php echo base_url() ?>uploads/avatar/<?php echo $membre->avatar ?>" class="pro-turtle " alt="user">
+                                            <small><?php echo $membre->name ?></small>                        
+                                        </td>
+                                        <td>
+                                            <small><?php echo $membre->club ?></small>
+                                        </td>
+
+                                        <td> <b ><?php echo $membre->scores ?></b> </td>
+                                      </tr>
+                                      
+                                 
+                                    <?php $c= $c + 1 ;  }   }else { echo "Il y a pas de données";} ?>
+                                </table>
+                            </div>
+                            </aside>
+                            </div>
+
+                            <!-- end scoring membres -->
+
+                             <!-- scoring membres AF -->
+                            <div class="col-lg-3 " >
+                                <aside class="sidebar static right" >
+                                <div class="widget">
+                                <h4 class="widget-title">TOP 10 Tunimateurs</h4>
+                                 <style type="text/css">
+                                        .pro-turtle {
+                                            object-fit: cover;
+                                            border-radius : 100% ;
+                                            object-position: 50% 50%;
+                                            width: 30px;
+                                            height: 30px;
+                                        }
+                                    </style> 
+                                    <table  class="table table-striped table-responsive" id="tableid" style="width: cover" >
+                                      <thead>
+                                          <th width="5%" >#
+                                          </th>
+                                          <th width="10%" >
+                                            Badge
+                                          </th>
+                                          <th>
+                                            Tunimateur
+                                          </th>
+                                          <th>
+                                            Club
+                                          </th>
+                                          
+                                     
+                                          <th>
+                                            Score
+                                          </th>
+                                          </thead>
+                                    <?php  if(!empty($mAF)){ $c=1 ;   foreach ($mAF as $membre ) { ?>
+                                      <tr>
+
+                                        <td  ><h4> <?php echo   $c   ?> </h4> </td>
+
+                                    
+                                        <td>
+                                             <?php   if ($c== 1) { ?>
+
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 7a8 8 0 1 1 0 16 8 8 0 0 1 0-16zm0 3.5l-1.323 2.68-2.957.43 2.14 2.085-.505 2.946L12 17.25l2.645 1.39-.505-2.945 2.14-2.086-2.957-.43L12 10.5zm1-8.501L18 2v3l-1.363 1.138A9.935 9.935 0 0 0 13 5.049L13 2zm-2 0v3.05a9.935 9.935 0 0 0-3.636 1.088L6 5V2l5-.001z" fill="rgba(241,196,14,1)"/></svg>
+
+                                            <?php } else if ($c== 2) { ?>
+
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 7a8 8 0 1 1 0 16 8 8 0 0 1 0-16zm0 3.5l-1.323 2.68-2.957.43 2.14 2.085-.505 2.946L12 17.25l2.645 1.39-.505-2.945 2.14-2.086-2.957-.43L12 10.5zm1-8.501L18 2v3l-1.363 1.138A9.935 9.935 0 0 0 13 5.049L13 2zm-2 0v3.05a9.935 9.935 0 0 0-3.636 1.088L6 5V2l5-.001z" fill="rgba(149,164,166,1)"/></svg>
+
+                                            <?php } else if ($c== 3) { ?>
+
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 7a8 8 0 1 1 0 16 8 8 0 0 1 0-16zm0 3.5l-1.323 2.68-2.957.43 2.14 2.085-.505 2.946L12 17.25l2.645 1.39-.505-2.945 2.14-2.086-2.957-.43L12 10.5zm1-8.501L18 2v3l-1.363 1.138A9.935 9.935 0 0 0 13 5.049L13 2zm-2 0v3.05a9.935 9.935 0 0 0-3.636 1.088L6 5V2l5-.001z" fill="rgba(155,77,9,1)"/></svg>
+
+                                            <?php   }  ?>
+                                        </td>
+                                         
+
+                                         <td>
+                                      
+                                         <a href="<?php echo base_url() ?>User/ProfileShow/<?php echo $membre->userId ?>"> <img src="<?php echo base_url() ?>uploads/avatar/<?php echo $membre->avatar ?>" class="pro-turtle " alt="user">
+                                            <small><?php echo $membre->name ?></small>                        
+                                        </td>
+                                        <td>
+                                            <small><?php echo $membre->club ?></small>
+                                        </td>
+
+                                        <td> <b ><?php echo $membre->scores ?></b> </td>
+                                      </tr>
+                                      
+                                 
+                                    <?php $c= $c + 1 ;  }   }else { echo "Il y a pas de données";} ?>
+                                </table>
+                            </div>
+                            </aside>
+                            </div>
+
+                            <!-- end scoring membres -->
+
                         </div>
                     </div>
                 </div>
