e0d1a748c912f5a897a16552d5a4df312ebf9ccd
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2021-01-22 15:37:45 +0100

    Update myClub.php

--- application/views/club/myClub.php
+++ application/views/club/myClub.php
@@ -329,7 +329,7 @@
                                             <?php }else{  $p = $projet->score  ;
 
                                                if($p->statut == 1 ){
-                                                echo "<b style='color:white;background-color: yellow'>En cours de validation manuelle</b>" ;
+                                                echo "<b style='color:white;background-color: orange'>En cours de validation manuelle</b>" ;
                                               }elseif($p->statut == 2 ){
                                                 echo "<b style='color:yellow'>tentative de tricherie</b>" ;
                                               }else
