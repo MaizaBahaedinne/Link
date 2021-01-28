4cfbbd99d3b5416ece5126cac53af70192f5b9dc
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2021-01-29 00:27:18 +0100

    Update dashboard.php

--- application/views/scoring/dashboard.php
+++ application/views/scoring/dashboard.php
@@ -770,7 +770,66 @@
                               <!-- end scoring vp Marketing -->
                 </div>
 
-                 <!-- end scoring assistant -->
+                 <!-- end scoring membre -->
+
+                 <div class="row"> 
+
+                              <!-- scoring vp Marketing -->
+                              <div class="col-lg-3 " >
+                                  <aside class="sidebar static right" >
+                                  <div class="widget">
+                                  <h4 class="widget-title">TOP 3 Président Marketing</h4>
+                                   <style type="text/css">
+                                          .pro-turtle {
+                                              object-fit: cover;
+                                              border-radius : 100% ;
+                                              object-position: 50% 50%;
+                                              width: 30px;
+                                              height: 30px;
+                                          }
+                                      </style> 
+                                       <div class="your-page" >
+                                      <table  class="table-responsive-xl" id="tableid" style="width: 100%" >
+                                        <thead>
+                                            
+                                            <th >
+                                            </th>
+                                            <th>
+                                              Tunimateus
+                                            </th>
+                                            
+                                       
+                                            <th>
+                                              S
+                                            </th>
+                                            </thead>
+                                      <?php  if(!empty($presidents)){ $c=1 ;   foreach ($presidents as $membre ) { ?>
+                                        <tr>
+
+                                          
+
+                                           <td>
+                                        
+                                           <a href="<?php echo base_url() ?>User/ProfileShow/<?php echo $membre->userId ?>"> <img src="<?php echo base_url() ?>uploads/avatar/<?php echo $membre->avatar ?>" class="pro-turtle " alt="user"></a>
+                                                                      
+                                          </td>
+                                          <td>
+                                              <small><b><?php echo $membre->name ?></b><br>
+                                              <?php echo $membre->club ?></small>
+                                          </td>
+
+                                          <td> <b ><?php echo $membre->scores ?></b> </td>
+                                        </tr>
+                                        
+                                   
+                                      <?php $c= $c + 1 ;  }   }else { echo "Il y a pas de données";} ?>
+                                  </table>
+                              </div>
+                              </div>
+                              </aside>
+                              </div>
+                              <!-- end scoring vp Marketing -->
+                            </div>
 
             </div>
         </div>
