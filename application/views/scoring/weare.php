a98bb6ad44040c05511f936f1618b281ca4a037c
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2021-02-19 18:14:12 +0100

    Update weare.php

--- application/views/scoring/weare.php
+++ application/views/scoring/weare.php
@@ -13,52 +13,54 @@
                         		<div class="widget">
                         		<h4 class="widget-title">Classement des We Are</h4>
                                 <div class="your-page" >
+                                    
                                     <style type="text/css">
-                                        table.tableizer-table {
-                                            font-size: 12px;
-                                            border: 1px solid #CCC; 
-                                            font-family: Arial, Helvetica, sans-serif;
-                                        } 
-                                        .tableizer-table td {
-                                            padding: 4px;
-                                            margin: 3px;
-                                            border: 1px solid #CCC;
-                                        }
-                                        .tableizer-table th {
-                                            background-color: #104E8B; 
-                                            color: #FFF;
-                                            font-weight: bold;
-                                        }
-                                    </style>
-                                    <table class="tableizer-table table table-responsive-xl" id="tableid" style="width: 100%" >
-                                    <thead><tr class="tableizer-firstrow"><th>#</th><th>We Are</th><th>score</th></tr></thead><tbody>
-                                     <tr><td>1</td><td>We Are Bizerte</td><td>29900</td></tr>
-                                     <tr><td>2</td><td>We Are Tozeur</td><td>24350</td></tr>
-                                     <tr><td>3</td><td>We Are Sfax</td><td>20600</td></tr>
-                                     <tr><td>5</td><td>We Are Médenine</td><td>15875</td></tr>
-                                     <tr><td>4</td><td>We Are Sousse</td><td>15625</td></tr>
-                                     <tr><td>7</td><td>We Are Ariana</td><td>15450</td></tr>
-                                     <tr><td>10</td><td>We Are Monastir</td><td>15350</td></tr>
-                                     <tr><td>6</td><td>We Are Kairouan</td><td>14750</td></tr>
-                                     <tr><td>9</td><td>We Are Jendouba</td><td>14725</td></tr>
-                                     <tr><td>14</td><td>We Are Nabeul</td><td>12300</td></tr>
-                                     <tr><td>8</td><td>We Are Kef</td><td>9350</td></tr>
-                                     <tr><td>11</td><td>We Are Mahdia</td><td>9100</td></tr>
-                                     <tr><td>15</td><td>We Are Zaghouan</td><td>9100</td></tr>
-                                     <tr><td>12</td><td>We Are Kasserine</td><td>8700</td></tr>
-                                     <tr><td>13</td><td>We Are Tunis</td><td>8150</td></tr>
-                                     <tr><td>17</td><td>We Are Manouba</td><td>7950</td></tr>
-                                     <tr><td>16</td><td>We Are Siliana</td><td>6350</td></tr>
-                                     <tr><td>18</td><td>We Are Gabes</td><td>5100</td></tr>
-                                     <tr><td>19</td><td>We Are Ben Arous</td><td>4100</td></tr>
-                                     <tr><td>20</td><td>We Are Béja</td><td>3400</td></tr>
-                                     <tr><td>21</td><td>We Are Gafsa</td><td>0</td></tr>
-                                     <tr><td>22</td><td>We Are Kébili</td><td>0</td></tr>
-                                     <tr><td>23</td><td>We Are Sidi Bouzid</td><td>0</td></tr>
-                                     <tr><td>24</td><td>We Are Tataouine</td><td>0</td></tr>
-                                    </tbody></table>
+                                            table.tableizer-table {
+                                                font-size: 12px;
+                                                border: 1px solid #CCC; 
+                                                font-family: Arial, Helvetica, sans-serif;
+                                            } 
+                                            .tableizer-table td {
+                                                padding: 4px;
+                                                margin: 3px;
+                                                border: 1px solid #CCC;
+                                            }
+                                            .tableizer-table th {
+                                                background-color: #104E8B; 
+                                                color: #FFF;
+                                                font-weight: bold;
+                                            }
+                                        </style>
+                                        <table class="tableizer-table tableizer-table table table-responsive-xl" id="tableid" style="width: 100%" >
+                                        <thead><tr class="tableizer-firstrow"><th>#</th><th>We Are</th><th>score</th></tr></thead><tbody>
+                                         <tr><td>1</td><td>We Are Bizerte</td><td>29900</td></tr>
+                                         <tr><td>2</td><td>We Are Tozeur</td><td>24350</td></tr>
+                                         <tr><td>3</td><td>We Are Sfax</td><td>20600</td></tr>
+                                         <tr><td>4</td><td>We Are Médenine</td><td>15875</td></tr>
+                                         <tr><td>5</td><td>We Are Sousse</td><td>15625</td></tr>
+                                         <tr><td>6</td><td>We Are Ariana</td><td>15450</td></tr>
+                                         <tr><td>7</td><td>We Are Monastir</td><td>15350</td></tr>
+                                         <tr><td>8</td><td>We Are Kairouan</td><td>14750</td></tr>
+                                         <tr><td>9</td><td>We Are Jendouba</td><td>14725</td></tr>
+                                         <tr><td>10</td><td>We Are Nabeul</td><td>12300</td></tr>
+                                         <tr><td>11</td><td>We Are Kef</td><td>9350</td></tr>
+                                         <tr><td>12</td><td>We Are Mahdia</td><td>9100</td></tr>
+                                         <tr><td>13</td><td>We Are Zaghouan</td><td>9100</td></tr>
+                                         <tr><td>14</td><td>We Are Kasserine</td><td>8700</td></tr>
+                                         <tr><td>15</td><td>We Are Tunis</td><td>8150</td></tr>
+                                         <tr><td>16</td><td>We Are Manouba</td><td>7950</td></tr>
+                                         <tr><td>17</td><td>We Are Siliana</td><td>6350</td></tr>
+                                         <tr><td>18</td><td>We Are Gabes</td><td>5100</td></tr>
+                                         <tr><td>19</td><td>We Are Ben Arous</td><td>4100</td></tr>
+                                         <tr><td>20</td><td>We Are Béja</td><td>3400</td></tr>
+                                         <tr><td>21</td><td>We Are Gafsa</td><td>0</td></tr>
+                                         <tr><td>22</td><td>We Are Kébili</td><td>0</td></tr>
+                                         <tr><td>23</td><td>We Are Sidi Bouzid</td><td>0</td></tr>
+                                         <tr><td>24</td><td>We Are Tataouine</td><td>0</td></tr>
+                                        </tbody></table>
 
 
+                            
                                 
                                  </div>
 					            </div>
