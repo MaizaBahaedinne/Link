879cfddab30c2c960f02b4ca67031d27f52d6efe
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2021-02-20 16:17:52 +0100

    Update leaders.php

--- application/views/Tunimateurs/leaders.php
+++ application/views/Tunimateurs/leaders.php
@@ -8,7 +8,7 @@
               <div class="col-lg-12 col-md-12 col-sm-6">
                 <aside class="sidebar">
                    <div class="widget">
-                    <h4 class="widget-title">Liste des leaders 
+                    <h4 class="widget-title">Liste des utilisateurs 
                        
                        </h4>
 
@@ -21,18 +21,19 @@
                        
                         <th>id</th>
                         <th>Nom et prénom</th>
-                        
+                        <?php if($SA == 1  ) { ?>
                         <th>Contact</th>
-                                               
-                    
-                 
+                        <?php }?>
+                       
+                        <th>Actif</th>
+
                         
                     </tr>
                     </thead>
 
                     <tbody>
                               
-              
+                </style>
                     
                     <?php
                     if(!empty($userRecords))
@@ -69,45 +70,50 @@
 
                         </td>
                         
-                       
+                        <?php if($SA == 1  ) { ?>
                         <td>
-                            <a HREF="mailto:<?php echo $record->email ?>">
+                            <A HREF="mailto:<?php echo $record->email ?>">
                                 <i class="fa fa-mail"></i><?php echo $record->email ?>
+                            </A> 
+                           <br> 
+                            <a href="<?php echo $record->facebook ?>">
+                                <i class="fab fa-facebook"></i> 
                             </a>
-                            <br> 
-                            <?php if($SA == 1 ) { ?>
-                                 <a href="tel:<?php echo $record->mobile ?>">
-                                    <i class="fa fa-phone"></i> <?php echo $record->mobile ?>
-                                </a>   
-                            <?php } ?>    
+                         | 
+                        <?php if($SA == 1 ) { ?>
+                             <a href="tel:<?php echo $record->mobile ?>">
+                                <i class="fa fa-phone"></i> <?php echo $record->mobile ?>
+                            </a>   
+                        <?php } ?>    
                          </td>
-                
+                         <?php } ?>   
 
                          
                         
                         
                         <td> 
                            
-                            <?php if($record->isDeleted == 0) { ?>
-                                <span class="btn btn-sm btn-primary" > Actif</span>
-                            <?php }   ?>
-                            <?php if($record->isDeleted == 3) { ?>
-                                <span class="btn btn-sm btn-warning " > non Actif</span>
-                            <?php } ?>
+                        <?php if($record->isDeleted == 0) { ?>
+                            <span class="btn btn-sm btn-primary" > Actif</span>
+                        <?php }   ?>
+                        <?php if($record->isDeleted == 3) { ?>
+                            <span class="btn btn-sm btn-warning " > non Actif</span>
+                        <?php } ?>
 
-                            <?php  if($record->isDeleted == 2) {  ?>
-                                <span class="btn btn-sm btn-danger"  > bloqué</span>
-                            <?php } ?>
+                        <?php  if($record->isDeleted == 2) {  ?>
+                            <span class="btn btn-sm btn-danger"  > bloqué</span>
+                        <?php } ?>
      
                            
                         </td>
 
                        
+                       
 
                     </tr>
                     <?php
                         }
-                    
+                    }
                     ?>
                     
                     </tbody>
