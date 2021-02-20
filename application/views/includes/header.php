0784918ba72eed9096721f63054740dc7ff56605
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2021-02-20 20:53:28 +0100

    Update header.php

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -97,6 +97,19 @@
   <script src="<?php echo base_url() ?>assets/js/jquery.eventCalendar.min.js"></script>
   <script src="<?php echo base_url() ?>assets/js/qrcode.js"></script>
   <script src="<?php echo base_url() ?>assets/js/html5-qrcode.min.js"></script>
+ 
+
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script >
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script >
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script >
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"></script >
+     <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.print.min.js"></script >
+
+  
+
+
+
+
 
         <!-- Global site tag (gtag.js) - Google Analytics -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-151434993-1"></script>
