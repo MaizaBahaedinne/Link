acfef7dc5105c6b63f03382ac69ba1815eb4219b
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2021-02-22 10:07:48 +0100

    Update profile.php

--- application/views/Tunimateurs/profile.php
+++ application/views/Tunimateurs/profile.php
@@ -265,16 +265,32 @@
                 <div class="central-meta">
                   <span class="create-post">Activités récentes<a href="#" title=""></a></span>
 
-                  <ul= class="nav nav-tabs" id="myTab" role="tablist">
-                      <li class="nav-item">
-                        <a class="nav-link active" href="#Participations" data-toggle="tab"  role="tab" aria-controls="home" aria-selected="true">Participations</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="#Taches" ata-toggle="tab" role="tab" aria-controls="profile" aria-selected="false">Taches</a>
-                      </li>
-                  </ul>
 
-                  <div class="tab-pane " id="Participations" role="tabpanel" aria-labelledby="Participations">
+                    <!-- Nav tabs -->
+                    <ul class="nav nav-tabs" id="myTab" role="tablist">
+                      <li class="nav-item">
+                        <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" id="messages-tab" data-toggle="tab" href="#messages" role="tab" aria-controls="messages" aria-selected="false">Messages</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" id="settings-tab" data-toggle="tab" href="#settings" role="tab" aria-controls="settings" aria-selected="false">Settings</a>
+                      </li>
+                    </ul>
+
+                    <!-- Tab panes -->
+                    <div class="tab-content">
+                      <div class="tab-pane active" id="home" role="tabpanel" aria-labelledby="home-tab">.1</div>
+                      <div class="tab-pane" id="profile" role="tabpanel" aria-labelledby="profile-tab">..2.</div>
+                      <div class="tab-pane" id="messages" role="tabpanel" aria-labelledby="messages-tab">.3..</div>
+                      <div class="tab-pane" id="settings" role="tabpanel" aria-labelledby="settings-tab">.4..</div>
+                    </div>
+ 
+
                       <ul class="activitiez"  >
                       
 
@@ -324,14 +340,7 @@
 
                          
                       </ul>
-                    </div>
-                      <div class="tab-pane " id="Taches" role="tabpanel" aria-labelledby="Taches">
-                      <ul class="activitiez"  >
-     
-                          Taches
-                         
-                      </ul>
-                    </div>
+                      
 
 
 
