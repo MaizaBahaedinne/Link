d2b88d2465b912c699e8ca1cf00104a84811175e
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2021-02-22 17:22:46 +0100

    Update evalutaionFondation.php

--- application/views/project/evalutaionFondation.php
+++ application/views/project/evalutaionFondation.php
@@ -84,13 +84,10 @@
                                  if($p->statut == 1 ){
                                     echo "<b style='color:white;background-color: orange'>En cours de validation manuelle</b>" ;
                                   }elseif($p->statut == 2 ){
-                                    echo "<b style='color:red';background-color: black'>tentative de tricherie</b>" ;
-                                   
-                            } 
-                            else 
-                                {
-                                   
-                             }   
+                                    echo "<b style='color:red';background-color: black'>tentative de tricherie</b>" ;    
+                            }
+                        } 
+                            
                             ?>
                         </td>                    
                     </tr>
