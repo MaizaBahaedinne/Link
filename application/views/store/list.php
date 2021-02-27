dc13b9d1b3f59f97e28b13382ecb5b5ec36e8590
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2021-02-27 21:25:22 +0100

    Update list.php

--- application/views/store/list.php
+++ application/views/store/list.php
@@ -11,7 +11,7 @@
 
              
               <!-- sidebar -->
-              <div class="col-lg-6">
+              <div class="col-lg-4">
                 <aside class="sidebar static right">
                    <div class="widget">
                     <h4 class="widget-title">Sweat à capuche rouge</h4>
@@ -35,10 +35,34 @@
                 </aside>
               </div><!-- sidebar -->
                             <!-- sidebar -->
-              <div class="col-lg-6">
+              <div class="col-lg-4">
                 <aside class="sidebar static right">
                    <div class="widget">
-                    <h4 class="widget-title">Sweat à capuche rouge</h4>
+                    <h4 class="widget-title">Sweat à capuche "Chaab El GO" rouge</h4>
+                    <div class="your-page">
+                      <div>
+                       <img src="" >
+                      </div>
+                        <br> 
+                        <label>Quantité</label>
+                        <input type="number" name="" >
+                        <label>Taille</label>
+                        <select >
+                          <option value="S">S</option>
+                          <option value="L">L</option>
+                          <option value="XL">XL</option>
+                          <option value="XXL">XXL</option>
+                        </select>
+                    </div>
+                  </div><!-- calendar -->
+                  <!-- recent links -->
+                </aside>
+              </div><!-- sidebar -->
+                            <!-- sidebar -->
+              <div class="col-lg-4">
+                <aside class="sidebar static right">
+                   <div class="widget">
+                    <h4 class="widget-title">Sweat à capuche "Chaab El GO" bleu</h4>
                     <div class="your-page">
                       <div>
                        <img src="" >
@@ -60,15 +84,14 @@
               </div><!-- sidebar -->
 
 
-
               <div class="col-lg-12">
                 <aside class="sidebar static right">
                    
-                    
-                    <div class="full-calendar">
-                      <input type="submit" name="" value="Reserver" class="btn btn-primary btn-block">
+                    <div class="widget">
+                      <div class="your-page">
+                        <input type="submit" name="" value="Reserver" class="btn btn-primary btn-block">
+                      </div>
                     </div>
-                 
                   <!-- recent links -->
                 </aside>
               </div><!-- sidebar -->
