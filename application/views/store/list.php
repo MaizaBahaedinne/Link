8aad8e39a14fb03f969e1047c9c3a55403d54540
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2021-02-27 21:23:28 +0100

    Update list.php

--- application/views/store/list.php
+++ application/views/store/list.php
@@ -4,17 +4,18 @@
         <div class="row">
 
 <div class="col-lg-12">
+  <form class="c-form" >  
 <div class="row merged20" id="page-contents">
                   
                   
 
-  <form class="c-form" >             
+             
               <!-- sidebar -->
-              <div class="col-lg-3">
+              <div class="col-lg-6">
                 <aside class="sidebar static right">
                    <div class="widget">
                     <h4 class="widget-title">Sweat à capuche rouge</h4>
-                    <div class="full-calendar">
+                    <div class="your-page">
                       <div>
                        <img src="" >
                       </div>
@@ -33,14 +34,12 @@
                   <!-- recent links -->
                 </aside>
               </div><!-- sidebar -->
-
-
-              <!-- sidebar -->
-              <div class="col-lg-3">
+                            <!-- sidebar -->
+              <div class="col-lg-6">
                 <aside class="sidebar static right">
                    <div class="widget">
-                    <h4 class="widget-title">Sweat à capuche "Chaab El GO" rouge</h4>
-                    <div class="full-calendar">
+                    <h4 class="widget-title">Sweat à capuche rouge</h4>
+                    <div class="your-page">
                       <div>
                        <img src="" >
                       </div>
@@ -61,32 +60,8 @@
               </div><!-- sidebar -->
 
 
-              <!-- sidebar -->
-              <div class="col-lg-3">
-                <aside class="sidebar static right">
-                   <div class="widget">
-                    <h4 class="widget-title">Sweat à capuche "Chaab El GO" Bleu</h4>
-                    <div class="full-calendar">
-                      <div>
-                       <img src="" >
-                      </div>
-                        <br> 
-                        <label>Quantité</label>
-                        <input type="number" name="" >
-                        <label>Taille</label>
-                        <select >
-                          <option value="S">S</option>
-                          <option value="L">L</option>
-                          <option value="XL">XL</option>
-                          <option value="XXL">XXL</option>
-                        </select>
-                    </div>
-                  </div><!-- calendar -->
-                  <!-- recent links -->
-                </aside>
-              </div><!-- sidebar -->
 
-              <div class="col-lg-3">
+              <div class="col-lg-12">
                 <aside class="sidebar static right">
                    
                     
@@ -98,10 +73,10 @@
                 </aside>
               </div><!-- sidebar -->
               
- </form>            
+          
               
             </div>
-
+ </form>  
           </div>
 
 
