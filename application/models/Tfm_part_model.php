c63b48c7f50dd1212a69e58fce2da71459eae99e
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2021-03-05 11:06:33 +0100

    Update Tfm_part_model.php

--- application/models/Tfm_part_model.php
+++ application/models/Tfm_part_model.php
@@ -109,11 +109,11 @@ class Tfm_part_model extends CI_Model
      */
     function TFMPartRythme($projetId  )
     {
-        $this->db->select(' count(BaseTbl.id) nbr , DATE(BaseTbl.dateInscrip) ');
+        $this->db->select(' count(BaseTbl.id) nbr , DATE(BaseTbl.dateInscrip) dateInscrip ');
         $this->db->from('tbl_tfm_part as BaseTbl');
         $this->db->where('BaseTbl.tfmId =',$projetId );
-        $this->db->group_by('    DATE(BaseTbl.dateInscrip )   ');
-        $this->db->order_by('   DATE(BaseTbl.dateInscrip)    ');
+        $this->db->group_by(' dateInscrip  ');
+        $this->db->order_by('  dateInscrip    ');
  
         $query = $this->db->get();
         $result = $query->result();        
