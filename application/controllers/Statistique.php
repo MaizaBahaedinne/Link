5df0e6089e1c1da19b99ef96337dcf91340fed93
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2021-03-05 18:48:55 +0100

    Update Statistique.php

--- application/controllers/Statistique.php
+++ application/controllers/Statistique.php
@@ -43,7 +43,7 @@ class Statistique extends BaseController {
         $data['partParclub'] = $this->Tfm_part_model->TFMPartByClubListing($ProjectId,2);
         $data['partParsexeH'] = $this->Tfm_part_model->TFMPartConfirmedBySexeHListing($ProjectId);
         $data['partParsexeF'] = $this->Tfm_part_model->TFMPartConfirmedBySexeFListing($ProjectId);
-        $data['countRythme'] = $this->Tfm_part_model->  ($ProjectId);
+        $data['countRythme'] = $this->Tfm_part_model->TFMPartRythme($ProjectId);
         $data['countINSNotConfirmed'] = $this->Tfm_part_model->TFMPartListing($ProjectId,2);
         $data['countINSConfirmed'] = $this->Tfm_part_model->TFMPartListing($ProjectId,1);
         $data['countINSConfirmed1'] = $this->Tfm_part_model->TFMPartListing1($ProjectId,2);
